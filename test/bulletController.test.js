35f5833ec66c89aa6fd5a89dac7773fee385bf78
Brian-Almendras-SC <brian.almendras@ucb.edu.bo>
2022-10-12 23:08:25 -0400

    add Commet for my team

--- test/bulletController.test.js
+++ test/bulletController.test.js
@@ -4,7 +4,7 @@ import bulletController from "../BulletController.js";
 describe('Testing controller for bullet.js `bulletController.js`',function(){
     let bullet_Controller
     beforeEach(()=>{
-        bullet_Controller=new bulletController("canvas", 180, "lightred", true,true)
+        bullet_Controller=new bulletController("canvas", 180, "lightred", true,true)// en el ultimo campo true para enmy y enemi controller ponen false, poor que tiene que entrar al if de esos constructores
     })
     it('should create ', () => {
         expect(bullet_Controller).toBeTruthy()
